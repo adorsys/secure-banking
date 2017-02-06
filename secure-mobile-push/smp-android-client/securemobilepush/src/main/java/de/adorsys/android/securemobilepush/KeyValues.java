6932cae82cafaa650c1c311cef7a3ff3e6ac11c1
Drilon Recica <dre@adorsys.de>
2017-02-06 20:45:17 +0100

    Created KeyValues class for storing String Constants

---
+++ secure-mobile-push/smp-android-client/securemobilepush/src/main/java/de/adorsys/android/securemobilepush/KeyValues.java
@@ -0,0 +1,8 @@
+package de.adorsys.android.securemobilepush;
+
+
+public class KeyValues {
+    public static final String INTENT_FILTER_SMS = "intentFilterSms";
+    public static final String KEY_SMS_SENDER = "smsSender";
+    public static final String KEY_SMS_MESSAGE = "smsMessage";
+}
