21fccf7d23d941f6468bc76aac949ddb9a1618e8
Drilon Recica <drilonreqica@gmail.com>
2017-02-15 10:39:18 +0100

    Created SmsConfig class for sms parsing initialization.

---
+++ secure-mobile-push/smp-android-client/securemobilepush/src/main/java/de/adorsys/android/securemobilepush/sms/SmsConfig.java
@@ -0,0 +1,12 @@
+package de.adorsys.android.securemobilepush.sms;
+
+import android.support.annotation.NonNull;
+
+class SmsConfig {
+    @NonNull
+    static String BEGIN_INDEX;
+    @NonNull
+    static String END_INDEX;
+    @NonNull
+    static String[] SMS_SENDER_NUMBERS;
+}
