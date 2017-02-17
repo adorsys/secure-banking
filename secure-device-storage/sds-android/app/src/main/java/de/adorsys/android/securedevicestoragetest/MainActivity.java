1a64ac8b980a12fb863db539f6ffa52f1e3b9965
Drilon Recica <drilonreqica@gmail.com>
2017-02-17 13:05:06 +0100

    Generating KeyPair

--- secure-device-storage/sds-android/app/src/main/java/de/adorsys/android/securedevicestoragetest/MainActivity.java
+++ secure-device-storage/sds-android/app/src/main/java/de/adorsys/android/securedevicestoragetest/MainActivity.java
@@ -1,7 +1,14 @@
 package de.adorsys.android.securedevicestoragetest;
 
-import android.support.v7.app.AppCompatActivity;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import de.adorsys.android.securedevicestorage.KeystoreTool;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView keyInfoTextView = (TextView) findViewById(R.id.key_info_text_view);
+        Button generateKeyButton = (Button) findViewById(R.id.generate_key_button);
+
+        generateKeyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (KeystoreTool.keyPairExists()) {
+                    Toast.makeText(MainActivity.this, "EXISTS", Toast.LENGTH_SHORT).show();
+                } else {
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
+                        KeystoreTool.generateKeyPair(MainActivity.this);
+                    }
+                }
+            }
+        });
     }
 }
--- secure-device-storage/sds-android/securedevicestorage/src/main/java/de/adorsys/android/securedevicestorage/KeystoreTool.java
+++ secure-device-storage/sds-android/securedevicestorage/src/main/java/de/adorsys/android/securedevicestorage/KeystoreTool.java
@@ -1,5 +1,65 @@
 package de.adorsys.android.securedevicestorage;
 
-public class KeystoreTool {
+import android.content.Context;
+import android.os.Build;
+import android.security.KeyPairGeneratorSpec;
+import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
+import android.util.Log;
 
+import java.math.BigInteger;
+import java.security.KeyPairGenerator;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.util.Calendar;
+
+import javax.security.auth.x500.X500Principal;
+
+import static android.content.ContentValues.TAG;
+
+public class KeystoreTool {
+    private static final String KEY_ALIAS = "androidKeyPair";
+    private static final String KEY_ENCRYPTION_ALGORITHM = "RSA";
+    private static final String KEY_KEYSTORE_NAME = "AndroidKeyStore";
+
+    public static boolean keyPairExists() {
+        return false;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR2)
+    public static void generateKeyPair(@NonNull Context context) {
+        try {
+            // Create new key if needed
+            if (keyPairExists()) {
+                Calendar start = Calendar.getInstance();
+                Calendar end = Calendar.getInstance();
+                end.add(Calendar.MONTH, 1);
+                KeyPairGeneratorSpec spec = new KeyPairGeneratorSpec.Builder(context)
+                        .setAlias(KEY_ALIAS)
+                        .setSubject(new X500Principal("CN=SecureDeviceStorage, O=Adorsys, C=Germany"))
+                        .setSerialNumber(BigInteger.ONE)
+                        .setStartDate(start.getTime())
+                        .setEndDate(end.getTime())
+                        .build();
+                KeyPairGenerator generator = KeyPairGenerator.getInstance(KEY_ENCRYPTION_ALGORITHM, KEY_KEYSTORE_NAME);
+                generator.initialize(spec);
+
+                generator.generateKeyPair();
+            }
+        } catch (Exception e) {
+            Log.e(TAG, Log.getStackTraceString(e));
+        }
+    }
+
+    public static void deleteKeyPair() {
+
+    }
+
+    public static PublicKey getPublicKey() {
+        return null;
+    }
+
+    public static PrivateKey getPrivateKey() {
+        return null;
+    }
 }
