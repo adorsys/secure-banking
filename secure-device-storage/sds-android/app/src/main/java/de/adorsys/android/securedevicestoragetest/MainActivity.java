c4553c3ee73c0ff6ec6dcd70d70e8e48040b8773
Drilon Recica <drilonreqica@gmail.com>
2017-02-18 23:58:34 +0100

    Updated style and fixed wrong import

--- secure-device-storage/sds-android/app/src/main/java/de/adorsys/android/securedevicestoragetest/MainActivity.java
+++ secure-device-storage/sds-android/app/src/main/java/de/adorsys/android/securedevicestoragetest/MainActivity.java
@@ -8,7 +8,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import de.adorsys.android.securedevicestorage.BuildConfig;
 import de.adorsys.android.securedevicestorage.KeystoreTool;
 import de.adorsys.android.securedevicestorage.SecurePreferences;
 
