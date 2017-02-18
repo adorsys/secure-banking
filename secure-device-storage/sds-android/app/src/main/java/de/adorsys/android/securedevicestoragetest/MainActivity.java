fee500193c3971f28792aae076c01eb0ad86175d
Drilon Recica <drilonreqica@gmail.com>
2017-02-18 19:14:01 +0100

    Set up test layout and logic in MainActivity

--- secure-device-storage/sds-android/app/src/main/java/de/adorsys/android/securedevicestoragetest/MainActivity.java
+++ secure-device-storage/sds-android/app/src/main/java/de/adorsys/android/securedevicestoragetest/MainActivity.java
@@ -3,9 +3,13 @@ package de.adorsys.android.securedevicestoragetest;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import de.adorsys.android.securedevicestorage.KeystoreTool;
+import de.adorsys.android.securedevicestorage.SecurePreferences;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,13 +19,28 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         final TextView keyInfoTextView = (TextView) findViewById(R.id.key_info_text_view);
-        Button generateKeyButton = (Button) findViewById(R.id.generate_key_button);
+        final Button generateKeyButton = (Button) findViewById(R.id.generate_key_button);
+
+        if (KeystoreTool.keyPairExists()) {
+            generateKeyButton.setText("Start Encryption/Decryption");
+        } else {
+            generateKeyButton.setText("Generate Key and Start Encryption/Decryption");
+        }
 
         generateKeyButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
-
+                    if (generateKeyButton.getText().toString().equals("Generate Key and Start Encryption/Decryption")) {
+                        generateKeyButton.setText("Start Encryption/Decryption");
+                    }
+                    SecurePreferences.setValue("TAG", "TEST", MainActivity.this);
+                    String encryptedMessage = SecurePreferences.getSecureValue("TAG", MainActivity.this);
+                    Log.d("LOGTAG", encryptedMessage);
+                    String decryptedMessage = SecurePreferences.getValue("TAG", MainActivity.this);
+                    Log.d("LOGTAG", decryptedMessage);
+                    keyInfoTextView.setText(getString(R.string.message_encrypted_decrypted,
+                            "Test", encryptedMessage, decryptedMessage));
                 }
             }
         });
