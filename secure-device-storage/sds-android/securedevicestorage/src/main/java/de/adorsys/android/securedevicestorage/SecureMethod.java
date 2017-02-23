80f0d7d90df50abc376b8b78a31b0f2ba8356c4b
Drilon Recica <dre@adorsys.de>
2017-02-23 11:16:09 +0100

    Simplified SecureMethod enum

--- secure-device-storage/sds-android/securedevicestorage/src/main/java/de/adorsys/android/securedevicestorage/SecureMethod.java
+++ secure-device-storage/sds-android/securedevicestorage/src/main/java/de/adorsys/android/securedevicestorage/SecureMethod.java
@@ -1,15 +1,5 @@
 package de.adorsys.android.securedevicestorage;
 
 public enum SecureMethod {
-    METHOD_ENCRYPT("methodEncrypt"), METHOD_HASH("methodHash");
-
-    private final String method;
-    SecureMethod(final String text) {
-        this.method = text;
-    }
-
-    @Override
-    public String toString() {
-        return method;
-    }
+    METHOD_ENCRYPT, METHOD_HASH
 }
