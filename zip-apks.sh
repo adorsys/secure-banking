5e22cbb0f92515919c080496611a850832106de0
Drilon Reçica <drilonreqica@gmail.com>
2017-04-12 22:05:31 +0200

    Removed unneeded Android related bash scripts

--- zip-apks.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-zip -r generated-apks.zip secure-device-storage/sds-android/app/build/outputs/apk/*.apk secure-mobile-push/smp-android-client/app/build/outputs/apk/*.apk sms-parser/android-sms-parser/app/build/outputs/apk/*.apk
-echo Packaged apks
