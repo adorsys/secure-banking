5fd4439b718a6d7d768dd9a6b779cda60424f4c2
Denys Golubiev <denys.golubiev@adorsys.de>
2017-04-25 12:01:42 +0200

    Move cryptoutils and envutils to separate repositories
    https://github.com/adorsys/envutils
    https://github.com/adorsys/cryptoutils

--- cryptoutils/encobject/src/main/java/org/adorsys/encobject/service/MissingKeystoreAlgorithmException.java
+++
@@ -1,8 +0,0 @@
-package org.adorsys.encobject.service;
-
-public class MissingKeystoreAlgorithmException extends Exception {
-	private static final long serialVersionUID = -8244399588062333573L;
-	public MissingKeystoreAlgorithmException(String message, Throwable cause) {
-		super(message, cause);
-	}
-}
--- cryptoutils/encobject/src/main/java/org/adorsys/encobject/service/MissingKeystoreProviderException.java
+++
@@ -1,8 +0,0 @@
-package org.adorsys.encobject.service;
-
-public class MissingKeystoreProviderException extends Exception {
-	private static final long serialVersionUID = 4941429144446435839L;
-	public MissingKeystoreProviderException(String message, Throwable cause) {
-		super(message, cause);
-	}
-}
